bbbbeff49b6d238c4d0b4713c023a7da2495f77c
AKF01.L <72521515+AKF01Ltd@users.noreply.github.com>
2020-10-29 05:53:04 +0700

    Update p.py

--- p.py
+++ p.py
@@ -1,28 +1,3 @@
-import itertools
-import threading
-import time
-import sys
- 
-done = False
- 
-#animasi loading
-def animate():
-    for c in itertools.cycle(['A', 'K', 'F', '.']):
-        if done:
-            break
-        sys.stdout.write('\rloading ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rDone!     ')
- 
-t = threading.Thread(target=animate)
-t.start()
- 
-#proses lama disini
- 
-time.sleep(10)
-done = True
-
 import requests, time
 print("""
 \33[93m\nNIRVANA CODER TEAM 
